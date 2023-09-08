6329bc477b9f76ca86cac7e3b4fdedf13527a859
aleoli05 <aloi05@gmail.com>
2023-09-08 15:48:37 -0300

    08092023 2 Commit

--- R/Install_Required_Packages.R
+++ R/Install_Required_Packages.R
@@ -1,44 +1,48 @@
+#' Install_Required_Packages
 #' Install all necessary packages for required commands.
 #' @export
-#' @param () No required parameters
+#' @param Packages is required parameter
 
 
 #' @examples
-#' Install_Required_Package()
+#' Install_Required_Packages(Packages=c('ALL'))
 #'
 
-Install_Required_Package <- function(x) {
-  # library(webinar.cpom)
-  install.packages("quantmod")
-  install.packages("PerformanceAnalytics")
-  install.packages("magrittr")
-  install.packages("fBasics")
-  install.packages("tidyverse")
-  install.packages("stringr")
-  install.packages("dplyr")
-  install.packages("neuralnet")
-  install.packages("zoo")
-  install.packages("forecast")
-  install.packages("timetk")
-  install.packages("moments")
-  install.packages("data.table")
-  install.packages("ggplot2")
-  install.packages("rvest")
-  install.packages("caret")
-  install.packages("readxl")
-  install.packages("writexl")
-  install.packages("portfolio.optimization")
-  install.packages("PortfolioAnalytics")
-  install.packages("ROI")
-  install.packages("fPortfolio")
-  install.packages("timeSeries")
-  install.packages("gridExtra")
-  install.packages("cowplot")
-  install.packages("portfolioBacktest")
-  install.packages("CVXR")
-  install.packages("MFDFA")
-  install.packages("DEoptim")
-  install.packages("rvest")
+Install_Required_Packages <- function(Packages) {
+  if(Packages==c('ALL')) {
+    x=x
+    # library(webinar.cpom)
+    install.packages("quantmod")
+    install.packages("PerformanceAnalytics")
+    install.packages("magrittr")
+    install.packages("fBasics")
+    install.packages("tidyverse")
+    install.packages("stringr")
+    install.packages("dplyr")
+    install.packages("neuralnet")
+    install.packages("zoo")
+    install.packages("forecast")
+    install.packages("timetk")
+    install.packages("moments")
+    install.packages("data.table")
+    install.packages("ggplot2")
+    install.packages("rvest")
+    install.packages("caret")
+    install.packages("readxl")
+    install.packages("writexl")
+    install.packages("portfolio.optimization")
+    install.packages("PortfolioAnalytics")
+    install.packages("ROI")
+    install.packages("fPortfolio")
+    install.packages("timeSeries")
+    install.packages("gridExtra")
+    install.packages("cowplot")
+    install.packages("portfolioBacktest")
+    install.packages("CVXR")
+    install.packages("MFDFA")
+    install.packages("DEoptim")
+    install.packages("rvest")
+  }
   x="Todos instalados"
   print(x)
 }
